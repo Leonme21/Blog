4508b018e77df260d761561f9f5b2374fa58f819
Miguel <mleonme21@gmail.com>
2026-04-28 22:18:30 -0500

    Correcion ruta program

--- PersonalBlog.Blazor/Program.cs
+++ PersonalBlog.Blazor/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7107/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://blog-production-8137.up.railway.app/") });
 builder.Services.AddScoped<PersonalBlog.Blazor.Services.AuthService>();
 
 await builder.Build().RunAsync();
